716d178dbf7163d9948b49130556a8c06715c291
zealot <0xmalloc@gmail.com>
2014-08-09 13:23:40 +0800

    技能分配CI
    需要配合 create_new_char  将strskill 变成binary

--- GameServer/DBAgent.cpp
+++ GameServer/DBAgent.cpp
@@ -382,7 +382,7 @@ bool CDBAgent::LoadUserData(string & strAccountID, string & strCharID, CUser *pU
 	pUser->m_oldy = pUser->m_cury;
 	pUser->m_oldz = pUser->m_curz;
 	pUser->m_dwTime = dbCommand->FetchUInt32(field++) + 1;
-	dbCommand->FetchString(field++, (char *)pUser->m_bstrSkill, sizeof(pUser->m_bstrSkill));
+	dbCommand->FetchBinary(field++, (char *)pUser->m_bstrSkill, sizeof(pUser->m_bstrSkill));
 	dbCommand->FetchBinary(field++, strItem, sizeof(strItem));
 	dbCommand->FetchBinary(field++, strSerial, sizeof(strSerial));
 	dbCommand->FetchUInt16(field++, sQuestCount);
